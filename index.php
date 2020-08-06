1be5ee44985b381e9cb51ea68414971bcb78d5f2
bellc709191 <60583538+bellc709191@users.noreply.github.com>
2020-08-07 10:54:21 +1200

    .

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
+    <meta name="description" content="Book, Review, Database">
+    <meta name="keywords" content="Books, Reading, Fiction, Non-Fiction, Genre, Reviews">
+    <meta name="author" content="Caitlin Bell">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Website Title goes here</title>
+    <title>Book Reviews</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
@@ -35,21 +35,21 @@
             </div>    <!-- / logo -->
         </a>
             
-            <h1>Website Name</h1>
-        </div>    <!-- / banner -->
-
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            <a href="index.html">Home</a> | 
-            <a href="page1.html">Page 1</a> | 
-            <a href="page2.html">Page 2</a> | 
-            <a href="page3.html">Page 3</a> | 
-            <a href="contact.html">Contact</a>
-        </div>    <!-- / nav -->        
+            <h1>Book Reviews</h1>
+        </div>    <!-- / banner -->       
         
         <div class="box side">
         
-            Side bar / search goes here
+            <h2>Side Bar</h2>
+                <p>items go here</p>
+            
+                <ul>
+                    <li>Here</li>
+                    <li>is</li>
+                    <li>a</li>
+                    <li>list</li>
+            
+                </ul>
             
         </div>
         
@@ -72,7 +72,7 @@
         
 
         <div class="box footer">
-            CC yourname 20XX
+            CC Caitlin Bell 2020
         </div>    <!-- / footer -->
     
     </div>  <!-- / wrapper  -->
